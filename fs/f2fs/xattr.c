a5fc39f54745c5172bf65ee3600308400298c3ad
tbalden <illespal@gmail.com>
2025-06-16 12:38:10 +0000

    tree: fix merge with new google a16 branch

--- fs/f2fs/xattr.c
+++ fs/f2fs/xattr.c
@@ -788,10 +788,11 @@ retry:
 same:
 	if (is_inode_flag_set(inode, FI_ACL_MODE)) {
 		inode->i_mode = F2FS_I(inode)->i_acl_mode;
-		inode->i_ctime = current_time(inode);
 		clear_inode_flag(inode, FI_ACL_MODE);
 	}
 
+	inode->i_ctime = current_time(inode);
+	f2fs_mark_inode_dirty_sync(inode, true);
 exit:
 	kfree(base_addr);
 	return error;
